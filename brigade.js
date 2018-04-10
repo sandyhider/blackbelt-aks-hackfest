734bd4c532f77109517754e7ff6440033c2553ca
Sandy Hider <sandy.hider@jhuapl.edu>
2018-04-10 10:37:21 -0400

    added my acr

--- brigade.js
+++ brigade.js
@@ -2,7 +2,7 @@ function kubeJobRunner (config, k) {
     k.storage.enabled = false
     k.image = "lachlanevenson/k8s-kubectl:v1.8.2"
     k.tasks = [
-        `kubectl set image deployment/heroes-web-deploy heroes-web-cntnr=<youracrhere>.azurecr.io/azureworkshop/rating-web:${config.get("imageTag")}`
+        `kubectl set image deployment/heroes-web-deploy heroes-web-cntnr=hiderrt1trainingacr.azurecr.io/azureworkshop/rating-web:${config.get("imageTag")}`
     ]
 }
 
