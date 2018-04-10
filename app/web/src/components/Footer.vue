a95e893ef1419e34677dfa0ffa7a87ac3fcdec33
Sandy Hider <sandy.hider@jhuapl.edu>
2018-04-10 11:31:46 -0400

    Sandy V

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Sandy Hider the IV
+        Sandy Hider the V
       </div>
       <div class="col-lg-6">
       </div>
